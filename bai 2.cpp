dd2f8261d38f5bf401463f802829b6dd4c9d30bb
nhutphuong496 <huynhphuong04092006@gmail.com>
2024-11-26 14:04:26 +0700

    Add files via upload

---
+++ bai 2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{ 
+	int n;
+	//NHAP SO NGUYEN TU 1-24//
+	printf("Nhap so nguyen chua thong tin gio: ");
+	scanf("%d",&n);
+	if (n<18)
+	{
+		printf("Have a good day!");
+	} else
+	{
+		printf("Have a good night!");
+	}
+	return 0;
+}
+ 	
