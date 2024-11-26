794a1961e495a3270f53dccce83851bb52085c6b
nhutphuong496 <huynhphuong04092006@gmail.com>
2024-11-26 15:25:04 +0700

    Add files via upload

--- bai 1.cpp
+++ bai 1.cpp
@@ -1,29 +1,17 @@
 #include <stdio.h>
-int main()
+int main ()
 {
-	//Buoc 2: Nhap vao mot so nguyen n nho hon 100
-	int n;
-	do
-	{
-		printf("\nNhap vao mot so nguyen duong n (n<100):");
-		scanf("%d",&n);
-		if (n <= 0 || n >= 100)
-		{
-			 printf("\nVui long nhap so nguyen duong n <= 100!");
-		}
-	}	
- 	while(n <= 0 ||n >= 100);
- 	
- 	//Buoc 5: Goi ham va kiem tra ket qua
- 	printNumbers(n);
- 	
- 	return 0;
+	//MSSV
+	printf ("\nMa So Sinh Vien: 110124153");
+	//Ho Ten
+	printf ("\nHo Va Ten: Huynh Nhut Phuong");
+	//Lop
+	printf ("\nMa Lop: DA24TTA");
+	//Nam Sinh
+	printf ("\nNam Sinh: 2006");
+	//Diem Xet Tuyen
+	printf ("\nDiem Xet Tuyen mon 1: 8");
+	printf ("\nDiem Xet Tuyen mom 2: 9");
+	printf ("\nDiem Xet Tuyen mom 3: 8");
+	return 0;
 }
-//Buoc 4: Xay dung ham chi  tiet ham
- void printNumbers (int n)
- {
- 	printf("\nCac so tu 1 den %d la:",n);
- 	for (int i = 1;i <= n; i++)
- 	
- }
-
