75cda2af4643f54d4cd6f02e40fcee34d8e9535b
nhutphuong496 <huynhphuong04092006@gmail.com>
2024-11-26 14:21:39 +0700

    Add files via upload

---
+++ bai 7.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a=100;
+	float b=8.95;
+	char c='C';
+	//In ra man hinh
+	printf("So nguyen:%d",a);
+	printf("\nSo thuc:%f",b);
+	printf("\nKy tu:%c",c);
+	return 0;
+}
